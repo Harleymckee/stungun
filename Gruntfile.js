8f0cb31f13def678705b1a364bff0a91aaf23cf9
harleymckee <Harley.McKee@gmail.com>
2014-12-20 17:14:16 -0700

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+
+//need to work on this guy
+/*
+   uglify: {
+      build: {
+        files: {
+          'client/_js/app.min.js': ['client/_js/*.js']
+        }
+      }
+    },
+
+ */
+
+
+
+
+  sass: {                              // Task
+    dist: {                            // Target
+      files: {                         // Dictionary of files
+        'client/_stylesheets/style.css': 'client/style.scss'       // 'destination': 'source'
+      }
+    }
+  },
+
+
+    //jshint: {
+ //     all: ['client/_js/**/*.js'] 
+//    }, 
+
+   /* cssmin: {
+      build: {
+        files: {
+          'client/_stylesheets/style.min.css': 'client/_stylesheets/style.css'
+        }
+      }
+    }, */
+
+
+
+    watch: {
+      css: {
+        files: ['client/**/*.css', 'client/**/*.scss'],
+        tasks: ['cssmin', 'sass']
+      },
+
+      js: {
+        files: ['client/**/*.js'],
+        tasks: ['sass']
+      }
+    },
+
+
+
+
+
+  	nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+
+
+
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      tasks: ['nodemon', 'watch']
+    }   
+
+
+
+
+  });
+
+
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+//  grunt.loadNpmTasks('grunt-contrib-jshint');
+//  grunt.loadNpmTasks('grunt-contrib-uglify');
+//  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+
+
+
+    grunt.registerTask('default', ['sass', 'concurrent']); //'jshint' 'uglify'
+
+};
