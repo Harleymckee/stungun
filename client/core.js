7d609a3d7469d451437a74b77d50e20d1a2d84ed
harleymckee <Harley.McKee@gmail.com>
2015-01-06 15:09:06 -0600

    panel logic

--- client/core.js
+++ client/core.js
@@ -8,3 +8,23 @@ var app = angular.module('stungun', []);
 
 	}]);
 
+	app.controller('PanelController', ['$scope', function($scope) {	
+
+		$scope.tab = 0;
+
+		this.selectTab = function(setTab) {
+
+		$scope.tab = setTab;
+		}
+
+
+	}]);
+
+
+	app.controller('BlogController', ['$scope', function($scope) {	
+
+
+
+
+	}]);
+
