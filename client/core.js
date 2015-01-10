82033802b862c9a543aa16cfa60c633edf6ef3c6
harleymckee <Harley.McKee@gmail.com>
2015-01-10 17:16:33 -0600

    blog fine tuning

--- client/core.js
+++ client/core.js
@@ -1,7 +1,20 @@
 var app = angular.module('stungun', ['firebase']);
 
 
+app.filter('backwards', function() {
+  return function(items) {
+    return items.slice().reverse();
+  };
+});
+
+
+
+
+
+
 	app.controller('StunController', ['$scope', function($scope) {	
+
+
 
 
 
@@ -95,8 +108,30 @@ for (var i = 0; i < tapeArray.length; i++) {
 	}]);
 
 
-	app.controller('BlogController', ['$scope', function($scope) {	
 
+/// make a factory for firebase ref
+
+
+	app.controller('BlogController', ['$scope',  '$firebase', function($scope, $firebase) {	
+
+
+		/* var blog1 = {body1: "dfasdfasfasdf",
+		date: "01/09/2015/19:36",
+		head: "safdasdf",
+		img1: "http://www.picgifs.com/music-graphics/music-graphics/rockstar/music-graphics-rockstar-288514.jpg" 
+						}; */
+
+
+
+
+var ref = new Firebase("https://stungun.firebaseio.com/blog");
+  var sync = $firebase(ref);
+
+	
+					
+var blogArray = sync.$asArray();
+
+this.blogger = blogArray; 
 
 
 
@@ -104,15 +139,20 @@ for (var i = 0; i < tapeArray.length; i++) {
 	}]);
 
 
-app.controller('BpostController', ['$scope', function($scope) {	
+app.controller('BpostController', ['$scope', '$firebase', function($scope, $firebase) {	
+
+
+
+var ref = new Firebase("https://stungun.firebaseio.com/blog");
+  var sync = $firebase(ref);
 
 	
 					
+var messagesArray = sync.$asArray();
+
 
 	this.blug = {};
-	var posts =[];
-
-
+	
 
 
 
@@ -126,9 +166,9 @@ var day = d.getDate();
 var dOutput = 
     (month<10 ? '0' : '') + month + '/' +
     (day<10 ? '0' : '') + day + '/' +
-    d.getFullYear() + '/' + 
-    d.getHours() + ':' +
-    d.getMinutes()
+    d.getFullYear() // + '/' + 
+   /* d.getHours() + ':' +
+    d.getMinutes() */
 
 
 
@@ -137,10 +177,11 @@ var dOutput =
 
 
 
-	posts.push(this.blug);
+	messagesArray.$add(this.blug);
 
-	console.log(posts);
-
+	//console.log(messagesArray);
+this.blug.img1 = "";
+this.blug.img2 = "";
 	this.blug = {};
 	}
 
@@ -168,9 +209,22 @@ var dOutput =
 			restrict: 'E',
 			templateUrl: 'the-blog.html',
 			controller: 'BlogController',
-			controllerAs: 'blug' 
+			controllerAs: 'blogCtrl' 
 
 		};
 	});
+
+		app.directive('blogPoster', function() {
+		return {
+
+			restrict: 'E',
+			templateUrl: 'blog-poster.html',
+			controller: 'BpostController',
+			controllerAs: 'blugCtrl' 
+
+		};
+	});
+
+
 
 
